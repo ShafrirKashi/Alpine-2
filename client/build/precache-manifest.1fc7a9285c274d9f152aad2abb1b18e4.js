9df121b1cf9aaaba5bba823cc972d3c0c6ad3b3d
ShafrirKashi <48563851+ShafrirKashi@users.noreply.github.com>
2022-06-13 21:24:05 +0300

    u

---
+++ client/build/precache-manifest.1fc7a9285c274d9f152aad2abb1b18e4.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "31e017676c516bc4345e",
+    "url": "/static/css/main.f72539c9.chunk.css"
+  },
+  {
+    "revision": "31e017676c516bc4345e",
+    "url": "/static/js/main.22ff6fd3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e6b1ab7650fb68ea097b",
+    "url": "/static/css/2.3b7da962.chunk.css"
+  },
+  {
+    "revision": "e6b1ab7650fb68ea097b",
+    "url": "/static/js/2.a7b0e789.chunk.js"
+  },
+  {
+    "revision": "112f1888bead15c00633e2dcd8001b3c",
+    "url": "/static/media/dropdownmen.112f1888.jpg"
+  },
+  {
+    "revision": "80320bf47130c45f3de25a3300fa02f7",
+    "url": "/static/media/dropdownwomen.80320bf4.jpg"
+  },
+  {
+    "revision": "542841c07102f52bf81949e831537ae3",
+    "url": "/static/media/dropdownkids.542841c0.jpg"
+  },
+  {
+    "revision": "ebe5bb1281baa60a3fa47b6a8ad3f205",
+    "url": "/static/media/dropdownequ.ebe5bb12.jpg"
+  },
+  {
+    "revision": "ddd7fb90e25cf33ae372c119f9e5cdc8",
+    "url": "/static/media/dropdownacce.ddd7fb90.jpg"
+  },
+  {
+    "revision": "e529942fee2dbf43df6985ad459e56fc",
+    "url": "/static/media/1.e529942f.png"
+  },
+  {
+    "revision": "a1c8d91648f172bf42adb248ffbc0cb2",
+    "url": "/static/media/apexlogo.a1c8d916.png"
+  },
+  {
+    "revision": "f70e6c9359550a76c9a43ade9444487c",
+    "url": "/static/media/quote.f70e6c93.png"
+  },
+  {
+    "revision": "aa196ecbd83f3114f1fab470f5f4d51a",
+    "url": "/static/media/quote2.aa196ecb.png"
+  },
+  {
+    "revision": "d5213044cfad1e4f4b5d1f3138752a80",
+    "url": "/static/media/ErasBold.d5213044.TTF"
+  },
+  {
+    "revision": "00c37a78f957ab5b14c2c7aabddace19",
+    "url": "/static/media/ErasMed.00c37a78.TTF"
+  },
+  {
+    "revision": "9f7891f4f192f1e8360990fadccccafb",
+    "url": "/static/media/ErasMed+.9f7891f4.TTF"
+  },
+  {
+    "revision": "4dacb160fc05b069f93fd5ed5b09b0be",
+    "url": "/static/media/TypoGraphica.4dacb160.otf"
+  },
+  {
+    "revision": "47620de5b73d9318a0542dd364ffb8fc",
+    "url": "/static/media/ErasLight.47620de5.TTF"
+  },
+  {
+    "revision": "d09549c1ab4a5947a007561521e45da3",
+    "url": "/static/media/SFProDisplay-Regular.d09549c1.ttf"
+  },
+  {
+    "revision": "7db578a4a6449b94c9ceccf2d12fed70",
+    "url": "/static/media/SFProDisplay-Ultralight.7db578a4.ttf"
+  },
+  {
+    "revision": "49689c08ce2ad9a3d749766b184eb1de",
+    "url": "/static/media/SFProDisplay-Thin.49689c08.ttf"
+  },
+  {
+    "revision": "42d79eba974955740a95cc0fb71ca247",
+    "url": "/static/media/SFProDisplay-Bold.42d79eba.ttf"
+  },
+  {
+    "revision": "28117b039b5512f6a685921297af1fee",
+    "url": "/static/media/SFProDisplay-Heavy.28117b03.ttf"
+  },
+  {
+    "revision": "18828951491f8bf21557f41e4b03b143",
+    "url": "/static/media/Trek.18828951.jpg"
+  },
+  {
+    "revision": "e5989955c2f3d6b05e28e67c73acb0b3",
+    "url": "/static/media/2.e5989955.jpg"
+  },
+  {
+    "revision": "a99ed0cdf6836c9686d7326d7e0b3385",
+    "url": "/static/media/4.a99ed0cd.jpg"
+  },
+  {
+    "revision": "65d7c1a21716fd5495796977957d6742",
+    "url": "/static/media/3.65d7c1a2.jpg"
+  },
+  {
+    "revision": "d229dc161557df4325414c3b2dfaeb8e",
+    "url": "/static/media/6.d229dc16.jpg"
+  },
+  {
+    "revision": "cbb7b905255565214bfa1ed207bfd934",
+    "url": "/static/media/22.cbb7b905.jpg"
+  },
+  {
+    "revision": "24a77a87920fe97c96f8624a67e0f2a1",
+    "url": "/static/media/5.24a77a87.jpg"
+  },
+  {
+    "revision": "14026687cbfa5afb77744e20f1a44002",
+    "url": "/static/media/Vipeak_black.14026687.png"
+  },
+  {
+    "revision": "4a4078ac6f29e361e18617371223d29b",
+    "url": "/static/media/Vipeak_white.4a4078ac.png"
+  },
+  {
+    "revision": "75baa348da5b317cc77bdf2df428f309",
+    "url": "/static/media/3051347.75baa348.jpg"
+  },
+  {
+    "revision": "b267d198e10dbd3e36e03f73e8282903",
+    "url": "/index.html"
+  }
+];
